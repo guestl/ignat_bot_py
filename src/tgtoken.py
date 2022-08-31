5cb93f0367f90547f7ba46a8152dd3266b425142
Boris N <boris.nikulin@gmail.com>
2022-08-31 12:40:25 +0300

    Delete tgtoken.py

--- src/tgtoken.py
+++
@@ -1,9 +0,0 @@
-import config
-
-
-if config.debug:
-    # test token
-    tgtoken = '911222847:AAE4zoulPi_5M5qRTEtj5gz3X3DjBZY4p1k'
-else:       
-    # prod token
-    tgtoken = '355907917:AAF_tYm1-WqqQLw49HzFsz69DpyJBa_LiYg'
